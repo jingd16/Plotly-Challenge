68d1945c991be17e119be5edbf4eb0fcea87d443
Jing Davies <jing.davies@hotmail.com>
2021-05-25 13:09:24 +1000

    bubble working

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-
+var metaSelection = d3.select("#sample-metadata")
 
 //STEP 1: Building Options Dropdown Box
 var selection = d3.select("#selDataset");
@@ -25,7 +25,14 @@ function buildOptions() {
 var sampleValues=[];
 var otuIDs = [];
 var otuLabels = [];
-var sampleID=[]
+var sampleID=[];
+
+var metaAge=[];
+var metaBbtype=[];
+var metaEthnicity=[];
+var metaGender=[];
+var metaLocation=[];
+var metaWfreq=[];
 function getData() {
     d3.json("samples.json").then((importedData) => {
         var data = importedData[0];
@@ -34,6 +41,7 @@ function getData() {
         //Take out sample as an array
         var samples = data.samples;
         //console.log(samples.length)
+        //var meta = data.metadata;
 
         // //Take out sample_value as an array & slice to top 10
         // sampleValues = samples.map(object => object.sample_values);
@@ -44,12 +52,16 @@ function getData() {
 
         sampleValues = samples.map(object => object.sample_values);
         sampleValues = sampleValues.map(object => object.slice(0,10));
-        //console.log(sampleValues);
         otuIDs = data.samples.map(object => object.otu_ids.slice(0,10));
-        //console.log(otuIDs)
         otuLabels = data.samples.map(object => object.otu_labels.slice(0,10));
-        //console.log(otuLabels)
         sampleID = data.names;
+
+        metaAge = data.metadata.map(object => object.age);
+        metaBbtype = data.metadata.map(object => object.bbtype);
+        metaEthnicity = data.metadata.map(object => object.ethnicity);
+        metaGender = data.metadata.map(object => object.gender);
+        metaLocation = data.metadata.map(object => object.location)
+        metaWfreq = data.metadata.map(object => object.wfreq);
 
         buildPlot(otuIDs, sampleValues, otuLabels, 0);
         
@@ -57,7 +69,10 @@ function getData() {
 };
 
 //STEP 3: build plot
-function buildPlot(otuIDs, sampleValues, otuLabels, sampleIndex) {
+function buildPlot(otuIDs, 
+                    sampleValues, 
+                    otuLabels, 
+                    sampleIndex) {
     
     //Convert ID to strings, in order to plot yaxis
     var strOtuIDs=[]
@@ -67,10 +82,13 @@ function buildPlot(otuIDs, sampleValues, otuLabels, sampleIndex) {
 
     //Create create yaxis names
     var IDs=strOtuIDs[sampleIndex].split(",")
+    var bubbleID=[]
     for (i=0; i<IDs.length; i++) {
+    bubbleID.push(IDs[i]);
     IDs[i] = `OTU${IDs[i]}`;
     };
     
+    // ====== Plot Bar Chart =======
     var trace1 = {
         x: sampleValues[sampleIndex],
         y: IDs,
@@ -82,18 +100,56 @@ function buildPlot(otuIDs, sampleValues, otuLabels, sampleIndex) {
     var data=[trace1];
 
     var layout = {
-            title: "Bar Chart"
+            title: " Belly Button Biodiversity",
+            height: 600,
+            width: 600
     };
 
     Plotly.newPlot("bar", data, layout);
+
+    //====== Plot bubble chart ======
+    var trace2 = {
+        x: IDs,
+        y: sampleValues[sampleIndex],
+        mode: 'markers',
+        marker: {
+          color: bubbleID,
+          opacity: [1, 0.8, 0.6, 0.4],
+          size: sampleValues[sampleIndex]
+        }
+      };
+      
+      var data2 = [trace2];
+      
+      var layout2 = {
+        title: 'Marker Size and Color',
+        showlegend: false,
+        height: 600,
+        width: 1200
+      };
+      
+      Plotly.newPlot('bubble', data2, layout2);
+
+
+
+
+
+    //=====Create MetaData list
+    metaSelection.append("ul")
+    metaSelection.append("li").text(`age: ${metaAge[sampleIndex]}`);
+    metaSelection.append("li").text(`bbtype: ${metaBbtype[sampleIndex]}`);
+    metaSelection.append("li").text(`ethnicity: ${metaEthnicity[sampleIndex]}`);
+    metaSelection.append("li").text(`gender: ${metaGender[sampleIndex]}`);
+    metaSelection.append("li").text(`location: ${metaLocation[sampleIndex]}`);
+    metaSelection.append("li").text(`wfreq: ${metaWfreq[sampleIndex]}`);  
 
 };
 
 buildOptions();
 
 function optionChanged(chosen) {
- 
     console.log(chosen);
+    //metaSelection.innerHTML("");
     //console.log(sampleValues);
 
     // d3.json("samples.json").then((importedData) => {
@@ -119,8 +175,11 @@ function optionChanged(chosen) {
 
         //console.log(IndexOption);
         };
+    
+    
 
     buildPlot(otuIDs, sampleValues, otuLabels, IndexOption);
+
 
     
 }
